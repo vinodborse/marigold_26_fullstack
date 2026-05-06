8f51e0dd19177f9e260970b393ac543b1755b9e8
vinod <borse002@gmail.com>
2026-05-06 09:24:51 +0530

    Added web app

---
+++ marigold_web_app_1/src/main/java/com/skillio/Student.java
@@ -0,0 +1,20 @@
+package com.skillio;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Student extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	        throws ServletException, IOException
+	    {
+			System.out.println("We are in Student class!!!!"); 
+	    }
+}
+
+
